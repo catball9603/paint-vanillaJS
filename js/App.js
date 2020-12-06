a0b7705ec6f5b42c70415d3381efd11ac9d2a2f1
Hokmah-ym <hokmahgrace@gamil.com>
2020-12-06 19:24:10 +0900

    pain.js 기능구현 완료

--- js/App.js
+++ js/App.js
@@ -1,22 +1,116 @@
-const clockContainer = document.querySelector('.js-clock');
-const clockTitle = clockContainer.querySelector('h1');
+const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRanges');
+const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
-function getTime() {
-  const date = new Date();
-  const mimutes = date.getMinutes();
-  const hours = date.getHours();
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_WIDTH = 1130;
+const CANVAS_HEIGHT = 700;
 
-  const seconds = date.getSeconds();
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+//paint모드시 배경이 투명이 되지 않게함.
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR; //이전 white로 변경된 것에 영향을 주지 않음
+ctx.lineWidth = 2.5;
 
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${mimutes < 10 ? `0${mimutes}` : mimutes}:${
-    seconds < 10 ? `0${seconds}` : seconds
-  }`;
+let painting = false; //값이 변하는 상수
+let filling = false;
+
+function stopPainting() {
+	painting = false;
 }
 
-//정의 된 함수를 실행
+function startPainting() {
+	painting = true;
+}
+
+function onMouseMove(e) {
+	const x = e.offsetX;
+	const y = e.offsetY;
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+}
+
+function handleColorClick(e) {
+	const color = e.target.style.backgroundColor;
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+}
+
+function handleRangeChange(e) {
+	const size = e.target.value;
+	ctx.lineWidth = size;
+	console.log(size);
+}
+
+function handleModeClick() {
+	if (filling === true) {
+		filling = false;
+		mode.innerText = 'Fill';
+	} else {
+		filling = true;
+		mode.innerText = 'Paint';
+	}
+}
+
+function handleCanvasClick() {
+	if (!filling) {
+		ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	}
+}
+function handleContextMenu(e) {
+	e.preventDefault();
+}
+
+function handleSaveClick() {
+	const image = canvas.toDataURL();
+	const link = document.createElement('a');
+	link.href = image;
+	link.download = 'myPaint';
+	link.click();
+}
+
+function paintingCanvas() {
+	if (canvas) {
+		//mouse가 canvas위치 감지
+		canvas.addEventListener('mousemove', onMouseMove);
+		//mouse버튼을 누르고 있을 때 painting 함수 실행
+		canvas.addEventListener('mousedown', startPainting);
+		//mouse버튼에서 띄었을 때
+		canvas.addEventListener('mouseup', stopPainting);
+		//mouse가 canvase를 벗어났을때
+		canvas.addEventListener('mouseleave', stopPainting);
+		canvas.addEventListener('click', handleCanvasClick);
+		canvas.addEventListener('contextmenu', handleContextMenu);
+	}
+}
+
 function init() {
-  getTime(); //시간을 먼저 받고
-  setInterval(getTime, 1000); // 1초 단위로 update되게 해준다.
+	//painting
+	paintingCanvas();
+	//color 선택
+	Array.from(colors).forEach((color) => color.addEventListener('click', handleColorClick));
+
+	if (range) {
+		range.addEventListener('input', handleRangeChange);
+	}
+
+	if (mode) {
+		mode.addEventListener('click', handleModeClick);
+	}
+	if (saveBtn) {
+		saveBtn.addEventListener('click', handleSaveClick);
+	}
 }
 
 init();
